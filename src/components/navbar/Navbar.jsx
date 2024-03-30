3eed3f68f657bcc926d3ebc8a2c2d6e218cfdc4c
Carlos Perales <carlosperales050@gmail.com>
2024-03-30 13:34:41 -0600

    fix : navBar

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -5,24 +5,19 @@ import { NavLink } from "react-router-dom";
 export default function Navbar() {
   return (
     <header>
-      <nav className="navbar-container">
-        <div className="navbar-left">
+      <nav className="flex justify-between items-center w-full fixed bg-gray-50/[.1]">
+        <div className="w-24  mx-5">
           <img
             src="https://cdn-icons-png.flaticon.com/512/63/63966.png"
             alt="logo"
+            className="sm:w-full w-1/2"
           />
         </div>
-        <div className="navbar-right">
-          <ul className="menu">
+        <div className="mx-9">
+          <ul className="flex gap-5 sm:gap-14 list-none capitalize">
             {routes.map((route) => (
               <li key={route.text}>
-                <NavLink
-                  className="link"
-                  style={({ isActive }) => ({
-                    color: isActive ? "#175AAD" : "#465D7A",
-                  })}
-                  to={route.to}
-                >
+                <NavLink className="text-white hover:underline" to={route.to}>
                   {route.text}
                 </NavLink>
               </li>
