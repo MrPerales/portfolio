7bb7f37039525f14356521cb0fbd27673f12ebd0
Carlos Perales <carlosperales050@gmail.com>
2024-03-30 17:18:35 -0600

    add: skills

---
+++ src/components/skills/Skills.jsx
@@ -0,0 +1,37 @@
+import React from "react";
+import AngularIcon from "../Icons/AngularIcon";
+import Html5Icon from "../Icons/Html5Icon";
+import ReactIcon from "../Icons/ReactIcon";
+import TailwindIcon from "../Icons/TailwindIcon";
+import JavaScriptIcon from "../Icons/JavaScriptIcon";
+import TypeScriptIcon from "../Icons/TypeScriptIcon";
+
+export default function Skills() {
+  return (
+    <section className="w-full   bg-gray-600 sm:bg-gray-700">
+      <div className="flex justify-center">
+        <div className="bg-gray-900 h-1 w-3/4 "></div>
+      </div>
+      <div className="flex justify-center flex-wrap items-center object-cover mt-5 sm:mt-10 space-x-5 sm:space-x-20">
+        <div className="w-10 h-10 sm:w-14 sm:h-14  opacity-50 hover:opacity-100">
+          <JavaScriptIcon />
+        </div>
+        <div className=" w-10 h-10 sm:w-14 sm:h-14  opacity-50 hover:opacity-100">
+          <TypeScriptIcon />
+        </div>
+        <div className="w-10 h-10 sm:w-14 sm:h-14  opacity-50 hover:opacity-100">
+          <Html5Icon />
+        </div>
+        <div className="w-10 h-10 sm:w-14 sm:h-14  opacity-50 hover:opacity-100">
+          <TailwindIcon />
+        </div>
+        <div className="w-10 h-10 sm:w-14 sm:h-14  opacity-50 hover:opacity-100">
+          <ReactIcon />
+        </div>
+        <div className="w-10 h-10 sm:w-14 sm:h-14  opacity-50 hover:opacity-100">
+          <AngularIcon />
+        </div>
+      </div>
+    </section>
+  );
+}
--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import Header from "../components/header/Header";
+import Skills from "../components/skills/Skills";
 
 export default function HomePage() {
   return (
     <main className="w-full h-screen bg-gray-600 sm:bg-gray-700">
       <Header />
+      <Skills></Skills>
     </main>
   );
 }
