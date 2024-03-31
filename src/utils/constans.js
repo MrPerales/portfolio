d4f77da6854810c34a0c9a9afeddcc26a025ab0e
Carlos Perales <carlosperales050@gmail.com>
2024-03-30 22:28:15 -0600

    add: screns

---
+++ src/utils/constans.js
@@ -0,0 +1,34 @@
+export const AngularScreenFakeStore = [
+  "src/assets/screenApps/Angular/FakeStore/FakeStoreHome.jpg",
+  "src/assets/screenApps/Angular/FakeStore/ProductDetail.jpg",
+  "src/assets/screenApps/Angular/FakeStore/storeCar.jpg",
+];
+export const AngularScreenTrello = [
+  "src/assets/screenApps/Angular/trello/trelloLogin.jpg",
+  "src/assets/screenApps/Angular/trello/trelloLoginMobile.jpg",
+  "src/assets/screenApps/Angular/trello/trelloRegister.jpg",
+  "src/assets/screenApps/Angular/trello/trelloBoards.jpg",
+  "src/assets/screenApps/Angular/trello/trelloBoard.jpg",
+];
+export const ReactScreenMovie = [
+  "src/assets/screenApps/React/movieApp/movieAppHome.jpg",
+  "src/assets/screenApps/React/movieApp/movieDetail.jpg",
+  "src/assets/screenApps/React/movieApp/categories.jpg",
+];
+export const ReactScreenTodo = [
+  "src/assets/screenApps/React/todoApp/todoAppHome.jpg",
+  "src/assets/screenApps/React/todoApp/todoAppAddTask.jpg",
+  "src/assets/screenApps/React/todoApp/editTask.jpg",
+];
+export const NativcScreenGame = [
+  "src/assets/screenApps/React Native/MemoryGame/home.jpg",
+  "src/assets/screenApps/React Native/MemoryGame/game.jpg",
+  "src/assets/screenApps/React Native/MemoryGame/game1.jpg",
+  "src/assets/screenApps/React Native/MemoryGame/score.jpg",
+];
+export const NativeScreenPokeDex = [
+  "src/assets/screenApps/React Native/Pokedex/pokedex.jpg",
+  "src/assets/screenApps/React Native/Pokedex/detail.jpg",
+  "src/assets/screenApps/React Native/Pokedex/favorites.jpg",
+  "src/assets/screenApps/React Native/Pokedex/account.jpg",
+];
