69e99b9f4141e8e6d67a41976c652dc36581f177
Carlos Perales <carlosperales050@gmail.com>
2024-04-02 18:28:46 -0600

    refactor: project component

--- src/utils/constans.js
+++ src/utils/constans.js
@@ -31,3 +31,154 @@ export const NativeScreenPokeDex = [
   "src/assets/screenApps/React Native/Pokedex/favorites.jpg",
   "src/assets/screenApps/React Native/Pokedex/account.jpg",
 ];
+export const projects = [
+  {
+    id: 1,
+    title: "Fake Store",
+    technologies: [
+      {
+        id: 1,
+        name: "TypeScript",
+      },
+      {
+        id: 2,
+        name: "taildwindCss",
+      },
+      {
+        id: 3,
+        name: "Angular",
+      },
+    ],
+    description:
+      " A e-commerce where do you can add the product to shopphing car and see their description, developed with angular and tailwindCss for the website design",
+    desktop: true,
+    screens: [
+      "src/assets/screenApps/Angular/FakeStore/FakeStoreHome.jpg",
+      "src/assets/screenApps/Angular/FakeStore/ProductDetail.jpg",
+      "src/assets/screenApps/Angular/FakeStore/storeCar.jpg",
+    ],
+    rowReverse: false,
+  },
+  {
+    id: 2,
+    title: "Trello Clon",
+    technologies: [
+      {
+        id: 1,
+        name: "TypeScript",
+      },
+      {
+        id: 2,
+        name: "taildwindCss",
+      },
+      {
+        id: 3,
+        name: "Angular",
+      },
+    ],
+    description:
+      " Trello clone,developed with Angular Cdk components to imitate the interface and consuming fake-trello-api for user verification and task posting",
+    desktop: true,
+    screens: [
+      "src/assets/screenApps/Angular/trello/trelloLogin.jpg",
+      "src/assets/screenApps/Angular/trello/trelloRegister.jpg",
+      "src/assets/screenApps/Angular/trello/trelloBoards.jpg",
+      "src/assets/screenApps/Angular/trello/trelloBoard.jpg",
+    ],
+    rowReverse: false,
+  },
+  {
+    id: 3,
+    title: "Movie App",
+    technologies: [
+      {
+        id: 4,
+        name: "JavaScript",
+      },
+      {
+        id: 5,
+        name: "React",
+      },
+    ],
+    description:
+      "A movie app developed with react framework,where would you find a lot of movies with their description, rating and other filters, using components and states to share the information got from API",
+    desktop: true,
+    screens: [
+      "src/assets/screenApps/React/movieApp/movieAppHome.jpg",
+      "src/assets/screenApps/React/movieApp/movieDetail.jpg",
+      "src/assets/screenApps/React/movieApp/categories.jpg",
+    ],
+    rowReverse: true,
+  },
+  {
+    id: 4,
+    title: "Todo App",
+    technologies: [
+      {
+        id: 4,
+        name: "JavaScript",
+      },
+      {
+        id: 5,
+        name: "React",
+      },
+    ],
+    description:
+      " A task management app , you can add ,edit and check the task.developed with React framework , using states and hooks to save the all task on local storage",
+    desktop: false,
+    screens: [
+      "src/assets/screenApps/React/todoApp/todoAppHome.jpg",
+      "src/assets/screenApps/React/todoApp/todoAppAddTask.jpg",
+      "src/assets/screenApps/React/todoApp/editTask.jpg",
+    ],
+    rowReverse: true,
+  },
+  {
+    id: 5,
+    title: "Pokedex",
+    technologies: [
+      {
+        id: 4,
+        name: "JavaScript",
+      },
+      {
+        id: 6,
+        name: "React Native",
+      },
+    ],
+    description:
+      " A pokedex app , where would you find all pokemons , save your Favorites pokes , and see their stats with a friendly interface ,developed with ReactNative",
+    desktop: false,
+    screens: [
+      "src/assets/screenApps/React Native/Pokedex/pokedex.jpg",
+      "src/assets/screenApps/React Native/Pokedex/detail.jpg",
+      "src/assets/screenApps/React Native/Pokedex/favorites.jpg",
+      "src/assets/screenApps/React Native/Pokedex/account.jpg",
+    ],
+    rowReverse: false,
+  },
+  {
+    id: 6,
+    title: "Memory Game",
+    technologies: [
+      {
+        id: 4,
+        name: "JavaScript",
+      },
+      {
+        id: 6,
+        name: "React Native",
+      },
+    ],
+    description:
+      " A memory game , is a classic board game but this is a virtual version developed with React Native where the objective and rulesare same that board game",
+    desktop: false,
+    screens: [
+      "src/assets/screenApps/React Native/MemoryGame/home.jpg",
+      "src/assets/screenApps/React Native/MemoryGame/game.jpg",
+      "src/assets/screenApps/React Native/MemoryGame/game.jpg",
+      "src/assets/screenApps/React Native/MemoryGame/score.jpg",
+    ],
+    rowReverse: false,
+  },
+];
